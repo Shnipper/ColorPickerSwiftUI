398f817a92ec451af69b14d6c942abf4efd34625
Voloshenko Evgeniu <95745884+Shnipper@users.noreply.github.com>
2022-05-08 15:07:46 +0300

    Initial Commit

---
+++ ColorPickerSwiftUI/ColorPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerSwiftUIApp.swift
+//  ColorPickerSwiftUI
+//
+//  Created by Евгений Волошенко on 08.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
